67ccb4fd7a23005edb4bb76f45785dbc0700c924
Michał Płużek <michalpluzek@gmail.com>
2022-05-30 12:24:39 +0200

    Fix typo

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(8000, () => console.log('Server is started on port 8000...'));
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
