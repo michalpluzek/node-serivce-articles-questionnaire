0fed36cfba39e00a8e43b3ed2cc15e1969cc0e94
Michał Płużek <michalpluzek@gmail.com>
2022-06-02 10:06:57 +0200

    Add news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 /* GET news page. */
 router.get('/', (req, res, next) => {
-  res.render('news', { title: 'News' });
+  const search = req.query.search || '';
+  const titleToSearch = new RegExp(search.trim(), 'i');
+
+  const findNews = News.find({ title: titleToSearch }).sort({ created: -1 });
+
+  findNews.exec((err, data) => {
+    res.render('news', { title: 'News', data, search });
+  });
 });
 
 module.exports = router;
