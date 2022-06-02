94eb5307b91523cfdfa1da8deb5d0362393dd68e
Michał Płużek <michalpluzek@gmail.com>
2022-06-02 10:41:49 +0200

    Add quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz');
 
 /* GET quiz page. */
 router.get('/', (req, res, next) => {
-  res.render('quiz', { title: 'Quiz' });
+  const show = !req.session.vote;
+
+  Quiz.find({}, (err, data) => {
+    let sum = 0;
+
+    data.forEach((item) => {
+      sum += item.vote;
+    });
+
+    res.render('quiz', { title: 'Quiz', data, show, sum });
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const id = req.body.quiz;
+
+  Quiz.findOne({ _id: id }, (err, data) => {
+    data.vote++;
+    data.save((err) => {
+      req.session.vote = 1;
+      res.redirect('/quiz');
+    });
+  });
 });
 
 module.exports = router;
